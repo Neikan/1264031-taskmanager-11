3e7165cac66860ca20d8597ca17dcfe18e96dfd7
Neikan <sladkov.e.m@outlook.com>
2020-04-08 13:36:53 +0300

    Generate filters based on an array of mock data

---
+++ src/mock/filters.js
@@ -0,0 +1,23 @@
+export const generateFilters = () => {
+  return [
+    {
+      name: `all`,
+      count: 42,
+    }, {
+      name: `overdue`,
+      count: 18,
+    }, {
+      name: `today`,
+      count: 18,
+    }, {
+      name: `favorites`,
+      count: 18,
+    }, {
+      name: `repeating`,
+      count: 18,
+    }, {
+      name: `archive`,
+      count: 18,
+    }
+  ];
+};
